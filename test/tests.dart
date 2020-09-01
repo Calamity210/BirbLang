2f6dd5eee1aed2959b118d37b9ee66cc1525ff64
Calamity210 <aayanraja210@gmail.com>
2020-08-31 21:25:32 -0400

    Deleted test.dart

--- test/tests.dart
+++
@@ -1,9 +0,0 @@
-import 'lexer_test.dart' as lexer_test;
-import 'parser_test.dart' as parser_test;
-import 'runtime_test.dart' as runtime_test;
-
-void main(List<String> args) {
-  lexer_test.main();
-  parser_test.main();
-  runtime_test.main();
-}
