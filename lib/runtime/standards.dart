c28f518748f5120662c9d667cb25d4abc5b48144
Calamity210 <aayanraja210@gmail.com>
2020-09-03 20:03:07 -0400

    random function added

--- lib/runtime/standards.dart
+++ lib/runtime/standards.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:Birb/utils/ast/ast_types.dart';
 import 'package:Birb/utils/exceptions.dart';
@@ -33,6 +34,7 @@ void initStandards(Runtime runtime, String path) async {
   registerGlobalFunction(runtime, 'screm', funcScrem);
   registerGlobalFunction(runtime, 'beep', funcBeep);
   registerGlobalFunction(runtime, 'exit', funcExit);
+  registerGlobalFunction(runtime, 'rand', funcRand);
   registerGlobalFunction(runtime, 'mock', funcMock);
   registerGlobalFunction(runtime, 'decodeJson', funcDecodeJson);
   registerGlobalFunction(runtime, 'encodeJson', funcEncodeJson);
@@ -146,6 +148,40 @@ AST funcExit(Runtime runtime, AST self, List args) {
 
   exit(exitAST.intVal);
   return null;
+}
+
+AST funcRand(Runtime runtime, AST self, List args) {
+  runtimeExpectArgs(args, [ASTType.AST_ANY]);
+
+  AST max = args[0];
+
+  if (max is IntNode) {
+    int randVal = Random().nextInt(max.intVal);
+    IntNode intNode = IntNode()..intVal = randVal..doubleVal = randVal.toDouble();
+    return intNode;
+  } else if (max is DoubleNode) {
+    double randVal = Random().nextDouble() * max.doubleVal;
+    DoubleNode doubleNode = DoubleNode()..doubleVal = randVal..intVal = randVal.toInt();
+
+    return doubleNode;
+  } else if (max is StringNode) {
+    Random rand = Random();
+    String result = '';
+
+    for (int i = 0; i < max.stringValue.length; i++) {
+      int range = max.stringValue.codeUnitAt(i) - 65;
+      int randVal = 65 + rand.nextInt(65);
+      result += String.fromCharCode(randVal);
+    }
+
+    StringNode strNode = StringNode()..stringValue = result;
+    return strNode;
+  } else if (max is BoolNode) {
+    BoolNode boolNode = BoolNode()..boolVal = Random().nextBool();
+    return boolNode;
+  }
+
+  throw UnexpectedTypeException('The rand method only takes [String, double, int, and bool] arugment types');
 }
 
 /**
