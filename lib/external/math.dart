a37dd24e2f46ed0ffcdd42602e19b2176928ce89
xxMrPHDxx <pian.minecraft1@gmail.com>
2020-09-14 00:51:37 +0800

    added trigo functions

--- lib/external/math.dart
+++ lib/external/math.dart
@@ -7,6 +7,11 @@ import 'package:Birb/utils/exceptions.dart';
 
 void registerMath(Runtime runtime) {
   registerGlobalFunction(runtime, 'rand', funcRand);
+  registerGlobalFunction(runtime, 'atan2', funcATan2);
+  for(final name in {'sin', 'cos', 'tan'}){
+    registerGlobalFunction(runtime, name, funcTrigo(name));
+    registerGlobalFunction(runtime, 'a$name', funcTrigo('a$name'));
+  }
 }
 
 ASTNode funcRand(Runtime runtime, ASTNode self, List<ASTNode> args) {
@@ -40,4 +45,98 @@ ASTNode funcRand(Runtime runtime, ASTNode self, List<ASTNode> args) {
   }
 
   throw const UnexpectedTypeException('The rand method only takes [String, double, int, and bool] argument types');
+}
+
+ASTNode funcATan2(Runtime runtime, ASTNode self, List<ASTNode> args){
+  runtimeExpectArgs(args, [ASTType.AST_ANY]);
+  final y = args[0], x = args[1];
+  switch(y.type) {
+    case ASTType.AST_INT: 
+      switch(x.type) {
+        case ASTType.AST_INT: return DoubleNode()..doubleVal = atan2(y.intVal, x.intVal);
+        case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = atan2(y.intVal, x.doubleVal);
+        default:
+      } break;
+    case ASTType.AST_DOUBLE: 
+      switch(x.type) {
+        case ASTType.AST_INT: return DoubleNode()..doubleVal = atan2(y.doubleVal, x.intVal);
+        case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = atan2(y.doubleVal, x.doubleVal);
+        default:
+      } break;
+    default:
+  }
+  throw const UnexpectedTypeException('The atan2 method only takes [double, int] argument types for argument 1 and 2');
+}
+
+ASTNode Function(Runtime, ASTNode, List<ASTNode>) funcTrigo(String name){
+  final ASTNode Function(Runtime, ASTNode, List<ASTNode>) Function(ASTNode Function(ASTNode)) func = (Function(ASTNode) callback) {
+    return (Runtime runtime, ASTNode self, List<ASTNode> args) {
+      runtimeExpectArgs(args, [ASTType.AST_ANY]);
+      final radian = args[0];
+      switch(radian.type){
+        case ASTType.AST_INT: 
+        case ASTType.AST_DOUBLE: 
+          return callback(radian);
+        default:
+      }
+      throw UnexpectedTypeException('The $name method only accept [double, int] argument types');
+    };
+  };
+  switch(name){
+    case 'sin': return func(funcSin);
+    case 'cos': return func(funcCos);
+    case 'tan': return func(funcTan);
+    case 'asin': return func(funcASin);
+    case 'acos': return func(funcACos);
+    case 'atan': return func(funcATan);
+  }
+  throw Exception('Undefined trigo function $name');
+}
+
+ASTNode funcSin(ASTNode radian){
+  switch(radian.type){
+    case ASTType.AST_INT: return DoubleNode()..doubleVal = sin(radian.intVal);
+    case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = sin(radian.doubleVal);
+    default: return NullNode();
+  }
+}
+
+ASTNode funcCos(ASTNode radian){
+  switch(radian.type){
+    case ASTType.AST_INT: return DoubleNode()..doubleVal = cos(radian.intVal);
+    case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = cos(radian.doubleVal);
+    default: return NullNode();
+  }
+}
+
+ASTNode funcTan(ASTNode radian){
+  switch(radian.type){
+    case ASTType.AST_INT: return DoubleNode()..doubleVal = tan(radian.intVal);
+    case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = tan(radian.doubleVal);
+    default: return NullNode();
+  }
+}
+
+ASTNode funcASin(ASTNode radian){
+  switch(radian.type){
+    case ASTType.AST_INT: return DoubleNode()..doubleVal = asin(radian.intVal);
+    case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = asin(radian.doubleVal);
+    default: return NullNode();
+  }
+}
+
+ASTNode funcACos(ASTNode radian){
+  switch(radian.type){
+    case ASTType.AST_INT: return DoubleNode()..doubleVal = acos(radian.intVal);
+    case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = acos(radian.doubleVal);
+    default: return NullNode();
+  }
+}
+
+ASTNode funcATan(ASTNode radian){
+  switch(radian.type){
+    case ASTType.AST_INT: return DoubleNode()..doubleVal = atan(radian.intVal);
+    case ASTType.AST_DOUBLE: return DoubleNode()..doubleVal = atan(radian.doubleVal);
+    default: return NullNode();
+  }
 }
