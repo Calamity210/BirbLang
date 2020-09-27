077612c57bc65bc04de441595f470af283badae4
xxMrPHDxx <pian.minecraft1@gmail.com>
2020-09-27 09:01:26 +0000

    added more info to the error thrown in 'lib/external/math.dart'

--- lib/external/math.dart
+++ lib/external/math.dart
@@ -19,104 +19,123 @@ void registerMath(Runtime runtime) {
   registerGlobalFunction(runtime, 'tan', funcTan);
 }
 
-ASTNode funcACos(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcACos(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final radian = args[0];
-     
-  if(radian is IntNode || radian is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = acos(radian is DoubleNode ? radian.doubleVal : radian.intVal);
+
+  if (radian is IntNode || radian is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal =
+          acos(radian is DoubleNode ? radian.doubleVal : radian.intVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The acos method only accept either double or int argument types');
+
+  throw UnexpectedTypeException(
+      'The acos method only accept either double or int argument types but got ${radian.type}');
 }
 
-ASTNode funcASin(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcASin(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final radian = args[0];
-     
-  if(radian is IntNode || radian is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = asin(radian is DoubleNode ? radian.doubleVal : radian.intVal);
+
+  if (radian is IntNode || radian is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal =
+          asin(radian is DoubleNode ? radian.doubleVal : radian.intVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The asin method only accept either double or int argument types');
+
+  throw UnexpectedTypeException(
+      'The asin method only accept either double or int argument types but got ${radian.type}');
 }
 
-ASTNode funcATan(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcATan(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final radian = args[0];
-     
-  if(radian is IntNode || radian is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = atan(radian is DoubleNode ? radian.doubleVal : radian.intVal);
+
+  if (radian is IntNode || radian is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal =
+          atan(radian is DoubleNode ? radian.doubleVal : radian.intVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The atan method only accept either double or int argument types');
+
+  throw UnexpectedTypeException(
+      'The atan method only accept either double or int argument types but got ${radian.type}');
 }
 
-ASTNode funcATan2(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcATan2(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY, ASTType.AST_ANY]);
   final y = args[0], x = args[1];
   final yVal = y is DoubleNode ? y.doubleVal : y.intVal;
   final xVal = x is DoubleNode ? x.doubleVal : x.intVal;
 
-  if((x is IntNode || x is DoubleNode) && (y is IntNode || y is DoubleNode)){
+  if ((x is IntNode || x is DoubleNode) && (y is IntNode || y is DoubleNode)) {
     final DoubleNode result = DoubleNode()..doubleVal = atan2(yVal, xVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The atan2 method only takes either double or int argument types for argument y and x');
+
+  throw UnexpectedTypeException(
+      'The atan2 method only takes either double or int argument types for argument y and x but got ${x.type} and ${y.type}');
 }
 
-ASTNode funcCos(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcCos(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final radian = args[0];
-     
-  if(radian is IntNode || radian is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = cos(radian is DoubleNode ? radian.doubleVal : radian.intVal);
+
+  if (radian is IntNode || radian is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal =
+          cos(radian is DoubleNode ? radian.doubleVal : radian.intVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The cos method only accept either double or int argument types');
+
+  throw UnexpectedTypeException(
+      'The cos method only accept either double or int argument types but got ${radian.type}');
 }
 
-ASTNode funcExp(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcExp(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final val = args[0];
 
-  if(val is IntNode || val is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = exp(val is DoubleNode ? val.doubleVal : val.intVal);
+  if (val is IntNode || val is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal = exp(val is DoubleNode ? val.doubleVal : val.intVal);
     return result;
   }
 
-  throw const UnexpectedTypeException('The exp method only takes either double or int argument types for argument val');
+  throw UnexpectedTypeException(
+      'The exp method only takes either double or int argument types for argument val but got ${val.type}');
 }
 
-ASTNode funcLn(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcLn(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final val = args[0];
 
-  if(val is IntNode || val is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = log(val is DoubleNode ? val.doubleVal : val.intVal);
+  if (val is IntNode || val is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal = log(val is DoubleNode ? val.doubleVal : val.intVal);
     return result;
   }
 
-  throw const UnexpectedTypeException('The ln method only takes either double or int argument types for argument val');
+  throw UnexpectedTypeException(
+      'The ln method only takes either double or int argument types for argument val but got ${val.type}');
 }
 
-ASTNode funcLog(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcLog(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY, ASTType.AST_ANY]);
   final base = args[0], val = args[1];
 
-  if((base is IntNode || base is DoubleNode) && (val is IntNode || val is DoubleNode)){
+  if ((base is IntNode || base is DoubleNode) &&
+      (val is IntNode || val is DoubleNode)) {
     final double up = log(val is DoubleNode ? val.doubleVal : val.intVal);
     final double down = log(base is DoubleNode ? base.doubleVal : base.intVal);
     final DoubleNode result = DoubleNode()..doubleVal = up / down;
     return result;
   }
 
-  throw const UnexpectedTypeException('The log method only takes either double or int argument types for argument base and val');
+  throw UnexpectedTypeException(
+      'The log method only takes either double or int argument types for argument base and val but got ${base.type} and ${val.type}');
 }
 
 ASTNode funcRand(Runtime runtime, ASTNode self, List<ASTNode> args) {
@@ -126,11 +145,15 @@ ASTNode funcRand(Runtime runtime, ASTNode self, List<ASTNode> args) {
 
   if (max is IntNode) {
     final int randVal = Random().nextInt(max.intVal);
-    final IntNode intNode = IntNode()..intVal = randVal..doubleVal = randVal.toDouble();
+    final IntNode intNode = IntNode()
+      ..intVal = randVal
+      ..doubleVal = randVal.toDouble();
     return intNode;
   } else if (max is DoubleNode) {
     final double randVal = Random().nextDouble() * max.doubleVal;
-    final DoubleNode doubleNode = DoubleNode()..doubleVal = randVal..intVal = randVal.toInt();
+    final DoubleNode doubleNode = DoubleNode()
+      ..doubleVal = randVal
+      ..intVal = randVal.toInt();
 
     return doubleNode;
   } else if (max is StringNode) {
@@ -149,29 +172,36 @@ ASTNode funcRand(Runtime runtime, ASTNode self, List<ASTNode> args) {
     return boolNode;
   }
 
-  throw const UnexpectedTypeException('The rand method only takes [String, double, int, and bool] argument types');
+  throw UnexpectedTypeException(
+      'The rand method only takes [String, double, int, and bool] argument types but got ${max.type}');
 }
 
-ASTNode funcSin(Runtime runtime, ASTNode self, List<ASTNode> args){
+ASTNode funcSin(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final radian = args[0];
-  
-  if(radian is IntNode || radian is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = sin(radian is DoubleNode ? radian.doubleVal : radian.intVal);
+
+  if (radian is IntNode || radian is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal =
+          sin(radian is DoubleNode ? radian.doubleVal : radian.intVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The sin method only accept either double or int argument types');
+
+  throw UnexpectedTypeException(
+      'The sin method only accept either double or int argument types but got ${radian.type}');
 }
 
-ASTNode funcTan(Runtime runtime, ASTNode self, List<ASTNode> args){  
+ASTNode funcTan(Runtime runtime, ASTNode self, List<ASTNode> args) {
   runtimeExpectArgs(args, [ASTType.AST_ANY]);
   final radian = args[0];
-     
-  if(radian is IntNode || radian is DoubleNode){
-    final DoubleNode result = DoubleNode()..doubleVal = tan(radian is DoubleNode ? radian.doubleVal : radian.intVal);
+
+  if (radian is IntNode || radian is DoubleNode) {
+    final DoubleNode result = DoubleNode()
+      ..doubleVal =
+          tan(radian is DoubleNode ? radian.doubleVal : radian.intVal);
     return result;
   }
-  
-  throw const UnexpectedTypeException('The tan method only accept either double or int argument types');
+
+  throw UnexpectedTypeException(
+      'The tan method only accept either double or int argument types but got ${radian.type}');
 }
